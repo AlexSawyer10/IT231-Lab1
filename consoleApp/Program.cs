b7cf5d398a7ad66a53f071e56825931b7d797599
AlexSawyer10 <asawyer37@southhills.edu>
2025-12-05 15:38:33 -0500

    fixing merge

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -15,10 +15,19 @@ class Program
         
         integer2 = int.Parse(Console.ReadLine()!);
 
-        doMath(integer1, integer2);
+        DoAdd(integer1, integer2);
+        
+        DoMultiply(integer1, integer2);
     }
 
-    public static void doMath (int int1,int int2)
+    public static void DoAdd (int int1,int int2)
+    {
+        int added = int1 + int2;
+        
+        Console.WriteLine(added);
+    }
+    
+    public static void DoMultiply (int int1,int int2)
     {
         int multiplied = int1 * int2;
         
