53e5ea869e3fe661983548d59beed7b2f8aac2a7
AlexSawyer10 <asawyer37@southhills.edu>
2025-12-05 15:03:50 -0500

    second commit

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int integer1;
+        int integer2;
+        
+        Console.WriteLine("Enter in integer one: ");
+        
+        integer1 = int.Parse(Console.ReadLine()!);
+        
+        Console.WriteLine("Enter in integer two: ");
+        
+        integer2 = int.Parse(Console.ReadLine()!);
+        
+        Console.WriteLine(integer1);
+        Console.WriteLine(integer2);
     }
 }
